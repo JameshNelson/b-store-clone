b899340722dd1aa44def3444910e5f8b2167ef75
JameshNelson <jamesh.nelson@att.net>
2017-07-20 15:23:34 -0600

    Delete config.js

--- server/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  MASSIVE_URI: "	postgres://ugmkbokq:c8ij80F-u-Ly_nAlf_Oso04NpAO0FQF6@babar.elephantsql.com:5432/ugmkbokq",
-  ENDPOINT:"jameshnelson.c6kaihe8jige.us-west-2.rds.amazonaws.com",
-  SESSION_SECRET: 'meatball',
-  DBPORT:"blizzardClone",
-  NAME:"Master",
-  PASSWORD:"groningen190"
-}
